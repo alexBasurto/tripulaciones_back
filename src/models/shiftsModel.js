c17549125481319d026305b76f65b1bbe9f224ad
LentinulaEdode <carmona.a.esther@gmail.com>
2024-01-03 18:35:24 +0100

    branch de shifts: crud y mvc

---
+++ src/models/shiftsModel.js
@@ -0,0 +1,33 @@
+import sequelize from "../config/sequelize.js";
+
+const shiftsModel = sequelize.define("tbShifts", {
+    idShift: {
+        type: sequelize.Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    idCompany: {
+        type: sequelize.Sequelize.INTEGER,
+        allowNull: false,
+    },
+    name: {
+        type: sequelize.Sequelize.STRING(45),
+        allowNull: false,
+    },
+    horarioEntrada: {
+        type: sequelize.Sequelize.TIME,
+        allowNull: true,
+    },
+    comments: {
+        type: sequelize.Sequelize.STRING(200),
+        allowNull: true,
+    },
+}, {
+    tableName: "tbShifts",
+});
+
+shiftsModel.belongsTo(companiesModel, {
+    foreignKey: 'idCompany',
+});
+
+export default shiftsModel;
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import employeesRouter from './employeesRouter.js';
+import shiftsRouter from './shiftsRouter.js';
+import companiesRouter from './companiesRouter.js';
 
 const router = Router();
 
 router.use('/employees', employeesRouter);
+router.use('./shifts', shiftsRouter);
+router.use('./companies', companiesRouter);
 
 export default router;
