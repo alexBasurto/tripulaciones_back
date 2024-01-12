ec96adf60c4a4baceac6128fdad20f7c394dd782
LentinulaEdode <carmona.a.esther@gmail.com>
2024-01-12 15:02:15 +0100

    cambios en el CSS PreMood, algunas líneas de cambios en PreMood jsx y añadidos svgs en carpeta public

--- client/src/pages/PreMoodTracker.jsx
+++ client/src/pages/PreMoodTracker.jsx
@@ -1,49 +1,97 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './PreMoodTracker.css';
+import muyMalIcon from '/shapes/muy-mal.svg';
+import malIcon from '/shapes/mal.svg';
+import normalIcon from '/shapes/normal.svg';
+import bienIcon from '/shapes/bien.svg';
+import muyBienIcon from '/shapes/muy-bien.svg';
 
-const PreMoodTracker = ({preMood, setPreMood}) => {
+const PreMoodTracker = ({preMood, setPreMood, activeComponent, setActiveComponent}) => {
   const moods = {
-    1: 'muy mal',
-    2: 'mal',
-    3: 'normal',
-    4: 'bien',
-    5: 'muy bien',
+    1: 'Muy mal',
+    2: 'Mal',
+    3: 'Normal',
+    4: 'Bien',
+    5: 'Muy bien',
+  };
+
+  const icons = {
+    "Muy mal": muyMalIcon,
+    "Mal": malIcon,
+    "Normal": normalIcon,
+    "Bien": bienIcon,
+    "Muy bien": muyBienIcon,
   };
 
   const manejarCambioDeslizador = (evento) => {
     setPreMood(evento.target.value);
   };
 
+  useEffect(() => {
+    document.body.className = `rastreador-de-emociones ${moods[preMood].replace(" ", "-")}`;
+    return () => {
+      document.body.className = 'rastreador-de-emociones';
+    };
+  }, [preMood]);
+
   return (
-    <div className={`rastreador-de-emociones ${moods[preMood].replace(" ", "-")}`}>
+    <div className={`main-container rastreador-de-emociones ${moods[preMood].replace(" ", "-")}`}>
       <header>
         <h4>Estado de ánimo</h4>
       </header>
       <main>
+      <div className="content-wrapper">
         <h2>¿Cómo te sentiste ayer al finalizar la jornada?</h2>
-        <div className={`estado-actual ${
-          moods[preMood].toLowerCase()
-        
-        }`}>
-          {estadoEmocion}
+        <div className={`rotating-image-container ${moods[preMood].toLowerCase()}`}>
+          <img className="rotating-image" src={icons[moods[preMood]]} alt={`Icon ${moods[preMood]}`} />
         </div>
-        <div className="contenedor-deslizador">
-          <input
-            type="range"
-            min="1"
-            max="5"
-            value={preMood}
-            onChange={manejarCambioDeslizador}
-            className="deslizador-emocion"
-          />
+        <div className={`estado-actual ${moods[preMood]}`}style={{ color: 'var(--estado-color)' }}>
+          {moods[preMood]}
+        </div>
+        <div className={`contenedor-deslizador ${moods[preMood].toLowerCase()}`}>
+      <input
+        type="range"
+        min="1"
+        max="5"
+        value={preMood}
+        onChange={manejarCambioDeslizador}
+        className='slider-white'
+        
+      />
           <div className="etiquetas-emocion">
             <span className="muymal">MUY MAL</span>
             <span className="muybien">MUY BIEN</span>
           </div>
+          <button
+              className="siguiente-button"
+              onClick={() => {
+                // Lógica para el botón de Siguiente
+                if (activeComponent === 'preMood') {
+                  setActiveComponent('feelings');
+                } else if (activeComponent === 'feelings') {
+                  setActiveComponent('reasons');
+                } else if (activeComponent === 'reasons') {
+                  setRegistered1(true);
+                  setTimeout(() => {
+                    setRegistered1(false);
+                    setActiveComponent('curMood');
+                  }, 3000);
+                } else if (activeComponent === 'curMood') {
+                  setRegistered2(true);
+                  setTimeout(() => {
+                    setRegistered2(false);
+                    setActiveComponent('ending');
+                  }, 3000);
+                }
+              }}
+            >
+              Siguiente
+            </button>
+        </div>
         </div>
       </main>
     </div>
   );
-  }
+}
 
 export default PreMoodTracker;
