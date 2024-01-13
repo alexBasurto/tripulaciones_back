9e1727d388b5a17b398e73ea37545fa401457cae
Alex Basurto <alexbasurto1@gmail.com>
2024-01-13 09:19:21 +0100

    Add dependencies and routes for Admin and SuperAdmin components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SessionProvider } from './context/SessionContext'
 import App from './App.jsx'
+import Admin from './Admin.jsx'
+import SuperAdmin from './SuperAdmin.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SessionProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/superadmin" element={<SuperAdmin />} />
+        </Routes>
+      </BrowserRouter>
     </SessionProvider>
   </React.StrictMode>,
 )
