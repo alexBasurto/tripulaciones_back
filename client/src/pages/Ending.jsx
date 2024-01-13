b23ba385722d2f122578f0fa249fad5e5d4e8aa0
Alex Basurto <alexbasurto1@gmail.com>
2024-01-13 08:44:56 +0100

    Add Ending.css and update Ending.jsx

--- client/src/pages/Ending.jsx
+++ client/src/pages/Ending.jsx
@@ -2,26 +2,37 @@ import { useState } from "react";
 import { createCommentApi } from "../utils/apiTripu";
 import { useSession } from "../context/SessionContext";
 
+import './Ending.css';
+
 const Ending = () => {
     const { session } = useSession();
     const [registered, setRegistered] = useState(false);
+    const [error, setError] = useState('');
+    const [isChecked, setIsChecked] = useState(true);
 
     const handleSubmit = () => {
-        setRegistered(true);
         const sendComment = async () => {
             try {
                 const comment = document.querySelector('input').value;
-                await createCommentApi(session.idEmployee, session.idCompany, comment, true);
+                await createCommentApi(session.idEmployee, session.idCompany, comment, isChecked);
             } catch (error) {
                 console.log(error);
             }
         }
-        sendComment();
-        document.querySelector('input').value = '';
-        setTimeout(() => {
-            setRegistered(false);
-        }, 2000);
-        
+
+        const comment = document.querySelector('input').value;
+        const regex = /^[^'";]{20,198}$/;
+        if (regex.test(comment) && comment.length > 20) {
+                setError('');
+                setRegistered(true);
+                sendComment();
+                document.querySelector('input').value = '';
+                setTimeout(() => {
+                    setRegistered(false);
+                }, 2000);
+        } else {
+            setError('El comentario debe tener entre 20 y 200 caracteres');
+        }
     }
 
     return (
@@ -30,11 +41,20 @@ const Ending = () => {
         <p>¡Ya has realizado la encuesta!</p>
         <p>¿Hay algo más que nos quieras comentar?</p>
         <input type="text" placeholder="Escribe aquí" />
+        <input type="checkbox" id="anonimousCheckbox" defaultChecked onChange={
+            () => {
+                setIsChecked(!isChecked);
+            }
+        } /> <label htmlFor="anonimousCheckbox">
+            Mantener anonimato
+        </label>
+        {!isChecked && <p className="alert">Tu comentario será enviado con tu identificación visible.</p>}
         <button
         onClick={() => {handleSubmit()}
         }
         >Enviar</button>
         {registered && <div className="blur"><p>Reporte enviado correctamente</p></div>}
+        <p>{error}</p>
         </div>
     );
     }
