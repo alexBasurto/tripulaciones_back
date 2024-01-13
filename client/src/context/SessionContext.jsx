dcc401797c4446c90c2011ce812811721e09aafd
Alex Basurto <alexbasurto1@gmail.com>
2024-01-13 09:39:29 +0100

    Remove commented out code in SessionContext.jsx

--- client/src/context/SessionContext.jsx
+++ client/src/context/SessionContext.jsx
@@ -27,32 +27,6 @@ const SessionProvider = ({ children }) => {
         }
     }, []);
 
-    /* useEffect(() => {
-        // Fetch latest voting data when fetchLatestVoting flag is true
-        if (fetchLatestVoting && session.data !== null && session.data !== "not-started") {
-            const getLatestVoting = async () => {
-                try {
-                    const response = await latestVotingApi(
-                        session.data.idEmployee,
-                        session.data.idCompany
-                    );
-                    const data = await response.json();
-                    // haz setSession con el ultimo voto, añadiendolo a la información que ya tiene
-                    setSession((prevSession) => ({
-                        ...prevSession,
-                        lastVoting: data,
-                    }));
-                    console.log('hecho2', data);
-                } catch (error) {
-                    console.log(error);
-                }
-            };
-            getLatestVoting();
-            // Reset the flag after fetching latest voting data
-            setFetchLatestVoting(false);
-        }
-    }, [fetchLatestVoting, session.data]); */
-
     return (
         <SessionContext.Provider value={{ session, setSession }}>
             {children}
