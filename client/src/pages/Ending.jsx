c95a25b183be44ed51c0bc04645309805d79deb8
Victor Campos <victor.cmp93@gmail.com>
2024-01-14 16:58:05 +0100

    cambios de ending

--- client/src/pages/Ending.jsx
+++ client/src/pages/Ending.jsx
@@ -23,40 +23,51 @@ const Ending = () => {
         const comment = document.querySelector('input').value;
         const regex = /^[^'";]{20,198}$/;
         if (regex.test(comment) && comment.length > 20) {
-                setError('');
-                setRegistered(true);
-                sendComment();
-                document.querySelector('input').value = '';
-                setTimeout(() => {
-                    setRegistered(false);
-                }, 2000);
+            setError('');
+            setRegistered(true);
+            sendComment();
+            document.querySelector('input').value = '';
+            setTimeout(() => {
+                setRegistered(false);
+            }, 2000);
         } else {
             setError('El comentario debe tener entre 20 y 200 caracteres');
         }
     }
 
+
     return (
         <div className="ending">
-        <h1>Ending</h1>
-        <p>¡Ya has realizado la encuesta!</p>
-        <p>¿Hay algo más que nos quieras comentar?</p>
-        <input type="text" placeholder="Escribe aquí" />
-        <input type="checkbox" id="anonimousCheckbox" defaultChecked onChange={
-            () => {
-                setIsChecked(!isChecked);
-            }
-        } /> <label htmlFor="anonimousCheckbox">
-            Mantener anonimato
-        </label>
-        {!isChecked && <p className="alert">Tu comentario será enviado con tu identificación visible.</p>}
-        <button
-        onClick={() => {handleSubmit()}
-        }
-        >Enviar</button>
-        {registered && <div className="blur"><p>Reporte enviado correctamente</p></div>}
-        <p>{error}</p>
+            <img src="cheque.png" alt="check" className="checklist" />
+            <p className="ending-txt1">Bienvenido a Moodly!</p>
+            <p className="ending-txt2">¡Ya has realizado la encuesta!</p>
+            <p className="ending-txt3">¿Hay algo más que nos quieras comentar?</p>
+            <div className="comment-input">
+                <input type="text" placeholder="Escribe aquí" />
+                <input type="checkbox" id="anonimousCheckbox" defaultChecked onChange={
+                    () => {
+                        setIsChecked(!isChecked);
+                    }
+                } /> <label htmlFor="anonimousCheckbox">
+                    Mantener anonimato
+                </label>
+                {!isChecked && <p className="alert">Tu comentario será enviado con tu identificación visible.</p>}
+                <div className="ending-btn">
+                    <button
+                        onClick={() => { handleSubmit() }
+                        }
+                    >Enviar</button>
+                </div>
+            </div>
+            {registered && <div className="blur">
+                <div className="blur-content">
+                    <img src="cheque.png" alt="check" className="checklist2" />
+                    <p className="p-txt">Reporte enviado correctamente</p>
+                </div>
+            </div>}
+            <p>{error}</p>
         </div>
     );
-    }
+}
 
 export default Ending;
