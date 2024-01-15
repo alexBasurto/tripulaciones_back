db8547834be9f892be3e5ce04bf4c3e71a344b0a
Victor Campos <victor.cmp93@gmail.com>
2024-01-15 11:54:39 +0100

    Update CSS styles for App and PreMoodTracker pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,17 +28,17 @@ const App = () => {
   useEffect(() => {
     if (session === null) {
       setActiveComponent('login');
+    } else {
+      if (session.lastWeekVotes[0] === 1) {
+        setActiveComponent('ending');
       } else {
-        if (session.lastWeekVotes[0] === 1) {
-          setActiveComponent('ending');
-        } else {
-          setFeelings([]);
-          setReasons([]);
-          setPreMood(3);
-          setCurMood(3);
-          setActiveComponent('preMood');
-        }
-    } 
+        setFeelings([]);
+        setReasons([]);
+        setPreMood(3);
+        setCurMood(3);
+        setActiveComponent('preMood');
+      }
+    }
   }
     , [session]);
 
@@ -73,49 +73,55 @@ const App = () => {
     <div className="app">
       {(activeComponent === 'feelings' || activeComponent === 'reasons') && (
         <div className="button-container">
-        <button
-          className="btn-back"
-          onClick={() => {
-            if (activeComponent === 'feelings') {
-              setFeelings([]);
-              setActiveComponent('preMood');
-            } else if (activeComponent === 'reasons') {
-              setActiveComponent('feelings');
-            }
-          }}
-        >
-          <span className="back-arrow"><img src="/icons/arrowIcon.svg" alt="Arrow" className='arrow-icon'/></span>
-          <span className="header-text">{activeComponent === 'feelings' ? 'Estado de ánimo' : 'Emociones'}</span>
-        </button>
-      </div>
+          <button
+            className="btn-back"
+            onClick={() => {
+              if (activeComponent === 'feelings') {
+                setFeelings([]);
+                setActiveComponent('preMood');
+              } else if (activeComponent === 'reasons') {
+                setActiveComponent('feelings');
+              }
+            }}
+          >
+            <span className="back-arrow"><img src="/icons/arrowIcon.svg" alt="Arrow" className='arrow-icon' /></span>
+            <span className="header-text">{activeComponent === 'feelings' ? 'Estado de ánimo' : 'Emociones'}</span>
+          </button>
+        </div>
       )}
 
       {(activeComponent == 'loading') && <div className='blur'>Cargando...</div>}
 
-        {activeComponent == 'login' && <Login setActiveComponent={setActiveComponent} /> }
-        {activeComponent == 'preMood' && <PreMoodTracker preMood={preMood} setPreMood={setPreMood} /> }
-        {activeComponent == 'feelings' && <Feelings preMood={preMood} feelings={feelings} setFeelings={setFeelings} /> }
-        {activeComponent == 'reasons' && <Reasons preMood={preMood} reasons={reasons} setReasons={setReasons} /> }
-        {registered1 && <div className='blur'>Registrado 1/2</div>}
-        {activeComponent == 'curMood' && <CurMoodTracker curMood={curMood} setCurMood={setCurMood} /> }
-        {registered2 && <div className='blur'>
+      {activeComponent == 'login' && <Login setActiveComponent={setActiveComponent} />}
+      {activeComponent == 'preMood' && <PreMoodTracker preMood={preMood} setPreMood={setPreMood} />}
+      {activeComponent == 'feelings' && <Feelings preMood={preMood} feelings={feelings} setFeelings={setFeelings} />}
+      {activeComponent == 'reasons' && <Reasons preMood={preMood} reasons={reasons} setReasons={setReasons} />}
+      {registered1 && <div className='blur'>
+        <div className='blur-content'>
+          <p className='p-txt'>Registrado 1/2</p>
+        </div>
+      </div>}
+      {activeComponent == 'curMood' && <CurMoodTracker curMood={curMood} setCurMood={setCurMood} />}
+      {registered2 && <div className='blur'>
+        <div className='blur-content'>
           <span>Registrado 2/2</span>
           <span>¡Gracias por participar!</span>
           <span>Tu racha es de {session.streak + 1} días</span>
           <StreakIcons />
-        
+
           <span>
             Recuerda que tu voto es anónimo y se registrará junto al de otras {session.employeesCount} personas
             {session.departmentName && ` del departamento ${session.departmentName}`}
             {session.branchName && `, de la sede ${session.branchName}`}
             {session.shiftName && `, del turno ${session.shiftName}`}
             .
-        </span>
+          </span>
+        </div>
 
-            </div>}
+      </div>}
 
-          
-        {activeComponent == 'ending' && <Ending/> }
+
+      {activeComponent == 'ending' && <Ending />}
 
       {/* <div>
           <p>TEST</p>
@@ -129,32 +135,32 @@ const App = () => {
         </div> */}
 
       {(activeComponent == 'preMood' || activeComponent == 'feelings' || activeComponent == 'reasons' || activeComponent == 'curMood') &&
-      
+
         <button
           className="btn-next text-button"
           onClick={() => {
-          if (activeComponent == 'preMood') {
-            setActiveComponent('feelings');
-          } else if (activeComponent == 'feelings') {
-            setActiveComponent('reasons');
-          } else if (activeComponent == 'reasons') {
-            setRegistered1(true);
-            setTimeout(() => {
-              setRegistered1(false);
-              setActiveComponent('curMood');
-            }, 3000);
-          } else if (activeComponent == 'curMood') {
-            setRegistered2(true);
-            setSendVote(true);
-            setTimeout(() => {
-              setRegistered2(false);
-              setActiveComponent('ending');
-            }, 3000);
+            if (activeComponent == 'preMood') {
+              setActiveComponent('feelings');
+            } else if (activeComponent == 'feelings') {
+              setActiveComponent('reasons');
+            } else if (activeComponent == 'reasons') {
+              setRegistered1(true);
+              setTimeout(() => {
+                setRegistered1(false);
+                setActiveComponent('curMood');
+              }, 3000);
+            } else if (activeComponent == 'curMood') {
+              setRegistered2(true);
+              setSendVote(true);
+              setTimeout(() => {
+                setRegistered2(false);
+                setActiveComponent('ending');
+              }, 3000);
+            }
           }
-        }
-        }>Siguiente</button>}
+          }>Siguiente</button>}
 
-        {session !== null && <LogoutButton />}
+      {session !== null && <LogoutButton />}
     </div>
   );
 };
